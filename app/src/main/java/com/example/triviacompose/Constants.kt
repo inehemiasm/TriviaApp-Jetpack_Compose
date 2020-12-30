9dccb2a75939a3aec1001c40f608ae3ed367ad29
Nehemias Miranda <mnehemias27@gmail.com>
2020-12-30 02:20:22 -0500

    Adding Demo .gif

--- app/src/main/java/com/example/triviacompose/Constants.kt
+++ app/src/main/java/com/example/triviacompose/Constants.kt
@@ -2,7 +2,6 @@ package com.example.triviacompose
 
 class Constants {
     companion object {
-        const val BASE_URL = "https://cat-fact.herokuapp.com"
         const val TRIVIA_BASE_URL = "https://opentdb.com"
     }
 }
